66317054107d557ba86b04a8cf1dfd1ad0bc5453
poojithavakada7-hue <poojithavakada7@gmail.com>
2026-01-08 17:30:52 +0530

    Add files via upload

---
+++ LocalVariables.java
@@ -0,0 +1,8 @@
+class LocalVariables
+{
+public static void main(String args[])
+{
+int a=4;
+System.out.println("The Local variable is "+a);
+}
+}
---
+++ StaticVariables.java
@@ -0,0 +1,9 @@
+class StaticVariables
+{
+static int a=4;
+public static void main(String args[])
+{
+
+System.out.println("The Static variable is "+a);
+}
+}
