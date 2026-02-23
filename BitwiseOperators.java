6ac60c4e99d3c569ae3514355dd011951135a49a
poojithavakada7-hue <poojithavakada7@gmail.com>
2026-02-23 16:52:03 +0530

    Add files via upload

---
+++ BitwiseOperators.java
@@ -0,0 +1,10 @@
+public class BitwiseOperators { 
+    public static void main(String[] args) { 
+      int x = 5, y = 3; 
+        System.out.println(x & y); 
+        System.out.println(x | y);
+	System.out.println(x ^ y);
+	System.out.println( ~ y);
+
+    } 
+}
