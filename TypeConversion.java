42786ad64a8d95c18cd67d5102cc0f0ce88cbf17
poojithavakada7-hue <poojithavakada7@gmail.com>
2026-02-26 16:52:12 +0530

    Add files via upload

---
+++ TypeConversion.java
@@ -0,0 +1,8 @@
+public class TypeConversion { 
+    public static void main(String[] args) { 
+        int num = 100; 
+        double d = num; // Automatic conversion from int to double 
+        System.out.println("Integer value: " + num); 
+        System.out.println("Double value: " + d); 
+    } 
+}
