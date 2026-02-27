fd2ace0c823899559246079e0a01d753bd1d4481
poojithavakada7-hue <poojithavakada7@gmail.com>
2026-02-27 17:06:22 +0530

    Add files via upload

---
+++ CharToInt.java
@@ -0,0 +1,8 @@
+public class CharToInt { 
+    public static void main(String[] args) { 
+        char ch = 'A';  
+        int num = ch;  
+        System.out.println("Character: " + ch); 
+        System.out.println("Converted Integer: " + num); 
+    } 
+}
