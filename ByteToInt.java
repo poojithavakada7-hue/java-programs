ed52d1ef0ff242babd6cdc29b85f5a31f64571e6
poojithavakada7-hue <poojithavakada7@gmail.com>
2026-02-28 17:07:35 +0530

    Add files via upload

---
+++ ByteToInt.java
@@ -0,0 +1,8 @@
+public class ByteToInt { 
+    public static void main(String[] args) { 
+        byte smallNumber = 11; 
+        int largerNumber = smallNumber;  
+        System.out.println("Byte value: " + smallNumber); 
+        System.out.println("Int value: " + largerNumber); 
+    } 
+}
