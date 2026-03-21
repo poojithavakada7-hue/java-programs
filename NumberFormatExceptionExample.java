3549c0c61d888b6222b2518ca98ddf1665b1111f
poojithavakada7-hue <poojithavakada7@gmail.com>
2026-03-21 20:20:19 +0530

    Add files via upload

---
+++ NumberFormatExceptionExample.java
@@ -0,0 +1,10 @@
+class NumberFormatExceptionExample {
+    public static void main(String[] args) {
+        try {
+            int num = Integer.parseInt("abc");
+            System.out.println(num);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid number format");
+        }
+    }
+}
